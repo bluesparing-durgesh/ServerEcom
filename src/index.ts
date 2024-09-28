2fdc24f87f8c12ccfd59ef2a54e9ced1ca4ac8be
durgesh <durgeshksm2000@gmail.com>
2024-09-29 00:06:17 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
-import userRouter from './routes/userRouter'
-import categoryRouter from './routes/categoryRouter'
-import productRouter from './routes/productRouter'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import userRouter from "./routes/userRouter";
+import categoryRouter from "./routes/categoryRouter";
+import productRouter from "./routes/productRouter";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
 app.use(cors());
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1/category",categoryRouter)
-app.use("/api/v1/product",productRouter)
+app.get("/",async(req: Request, res: Response) => {
+  return res.send("hello");
+});
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/product", productRouter);
 connectDB();
 
 app.listen(PORT, () => {
